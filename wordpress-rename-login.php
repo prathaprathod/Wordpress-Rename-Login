a81e93498ac47ec4e9f40a1c911464187abfaa90
Prathap Rathod <prathap0144@gmail.com>
2025-05-16 20:51:49 +0530

    Update wordpress-rename-login.php

--- wordpress-rename-login.php
+++ wordpress-rename-login.php
@@ -1,14 +1,11 @@
 <?php
-
 /*
  * Plugin Name: Wordpress Rename Login
  * Plugin URI:  https://wordpress.org/plugins/wordpress-rename-login/
- * Description: Wordpress Rename Login You Can Change Your Login url to anyone.
- * Version:     1.1.0
+ * Description: Wordpress Rename Login - You Can Change Your Login URL and restrict admin access by user roles.
+ * Version:     1.2.0
  * Author:      Prathap Rathod
  * Text Domain: wordpress-rename-login
- * Domain Path:
- * Network:
  * License:     GPL-3.0+
  */
 
@@ -31,6 +28,10 @@ class WP_Rename_Login {
 		// Admin settings
 		add_action( 'admin_menu', array( $this, 'add_settings_page' ) );
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
+
+		// Hide admin bar and block admin access
+		add_action( 'after_setup_theme', array( $this, 'maybe_hide_admin_bar' ) );
+		add_action( 'admin_init', array( $this, 'maybe_block_admin_access' ) );
 	}
 
 	public function plugins_loaded() {
@@ -38,23 +39,19 @@ class WP_Rename_Login {
 
 		load_plugin_textdomain( 'wordpress-rename-login' );
 
-		if (
-			! is_multisite() && (
-				strpos( $_SERVER['REQUEST_URI'], 'wp-signup' ) !== false ||
-				strpos( $_SERVER['REQUEST_URI'], 'wp-activate' ) !== false
-			)
-		) {
+		if ( ! is_multisite() && (
+			strpos( $_SERVER['REQUEST_URI'], 'wp-signup' ) !== false ||
+			strpos( $_SERVER['REQUEST_URI'], 'wp-activate' ) !== false
+		)) {
 			wp_die( __( 'This feature is not enabled.', 'wordpress-rename-login' ) );
 		}
 
 		$request = parse_url( $_SERVER['REQUEST_URI'] );
 
 		if ( (
-				strpos( $_SERVER['REQUEST_URI'], 'wp-login.php' ) !== false ||
-				untrailingslashit( $request['path'] ) === site_url( 'wp-login', 'relative' )
-			) &&
-			! is_admin()
-		) {
+			strpos( $_SERVER['REQUEST_URI'], 'wp-login.php' ) !== false ||
+			untrailingslashit( $request['path'] ) === site_url( 'wp-login', 'relative' )
+		) && ! is_admin() ) {
 			$this->wp_login_php = true;
 			$_SERVER['REQUEST_URI'] = $this->user_trailingslashit( '/' . str_repeat( '-/', 10 ) );
 			$pagenow = 'index.php';
@@ -125,9 +122,54 @@ class WP_Rename_Login {
 			'sanitize_callback' => 'sanitize_title',
 			'default' => $this->default_slug
 		) );
+
+		register_setting( 'wp_rename_login_settings', 'wp_rename_login_hide_admin_bar_roles', array(
+			'type' => 'array',
+			'sanitize_callback' => array( $this, 'sanitize_roles' ),
+			'default' => array()
+		) );
+
+		register_setting( 'wp_rename_login_settings', 'wp_rename_login_block_admin_roles', array(
+			'type' => 'array',
+			'sanitize_callback' => array( $this, 'sanitize_roles' ),
+			'default' => array()
+		) );
+	}
+
+	public function sanitize_roles( $roles ) {
+		return is_array( $roles ) ? array_map( 'sanitize_text_field', $roles ) : array();
+	}
+
+	public function maybe_hide_admin_bar() {
+		if ( is_user_logged_in() ) {
+			$current_user = wp_get_current_user();
+			$roles_to_hide = get_option( 'wp_rename_login_hide_admin_bar_roles', array() );
+
+			foreach ( $roles_to_hide as $role ) {
+				if ( in_array( $role, (array) $current_user->roles, true ) ) {
+					show_admin_bar( false );
+					break;
+				}
+			}
+		}
+	}
+
+	public function maybe_block_admin_access() {
+		if ( is_user_logged_in() && is_admin() && ! defined( 'DOING_AJAX' ) ) {
+			$current_user = wp_get_current_user();
+			$roles_to_block = get_option( 'wp_rename_login_block_admin_roles', array() );
+
+			foreach ( $roles_to_block as $role ) {
+				if ( in_array( $role, (array) $current_user->roles, true ) ) {
+					wp_redirect( home_url() );
+					exit;
+				}
+			}
+		}
 	}
 
 	public function settings_page_html() {
+		$all_roles = wp_roles()->roles;
 		?>
 		<div class="wrap">
 			<h1>Rename Login Settings</h1>
@@ -139,9 +181,39 @@ class WP_Rename_Login {
 						<th scope="row">New Login Slug</th>
 						<td>
 							<input type="text" name="wp_rename_login_slug" value="<?php echo esc_attr( get_option( 'wp_rename_login_slug', $this->default_slug ) ); ?>" />
-							<p class="description">Example: If you enter <strong>secure-login</strong>, your new login URL will be <code><?php echo home_url(); ?>/login123</code></p>
+							<p class="description">Example: If you enter <strong>secure-login</strong>, your new login URL will be <code><?php echo home_url(); ?>/secure-login</code></p>
 						</td>
 					</tr>
+
+					<tr valign="top">
+						<th scope="row">Hide Admin Bar For Roles</th>
+						<td>
+							<?php
+							$hide_roles = get_option( 'wp_rename_login_hide_admin_bar_roles', array() );
+							foreach ( $all_roles as $role_key => $role_data ) : ?>
+								<label>
+									<input type="checkbox" name="wp_rename_login_hide_admin_bar_roles[]" value="<?php echo esc_attr( $role_key ); ?>" <?php checked( in_array( $role_key, $hide_roles ) ); ?> />
+									<?php echo esc_html( $role_data['name'] ); ?>
+								</label><br>
+							<?php endforeach; ?>
+						</td>
+					</tr>
+
+					<tr valign="top">
+						<th scope="row">Block wp-admin Access For Roles</th>
+						<td>
+							<?php
+							$block_roles = get_option( 'wp_rename_login_block_admin_roles', array() );
+							foreach ( $all_roles as $role_key => $role_data ) : ?>
+								<label>
+									<input type="checkbox" name="wp_rename_login_block_admin_roles[]" value="<?php echo esc_attr( $role_key ); ?>" <?php checked( in_array( $role_key, $block_roles ) ); ?> />
+									<?php echo esc_html( $role_data['name'] ); ?>
+								</label><br>
+							<?php endforeach; ?>
+							<p class="description">Selected roles will be redirected to the homepage when trying to access wp-admin.</p>
+						</td>
+					</tr>
+
 				</table>
 				<?php submit_button(); ?>
 			</form>
